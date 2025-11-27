e6fc3c315ea5608f1aaea75f78871a3a74411b3d
Rishik Hombal <hombalrishik@gmail.com>
2025-11-27 14:23:56 -0800

    Update iscsi-bootstrap.sh

--- infrastructure/node-bootstrap/iscsi/iscsi-bootstrap.sh
+++ infrastructure/node-bootstrap/iscsi/iscsi-bootstrap.sh
@@ -8,6 +8,9 @@ if ! dpkg -s open-iscsi >/dev/null 2>&1; then
   apt-get install -y open-iscsi
 fi
 
+ln -sf /usr/bin/iscsiadm /usr/sbin/iscsiadm
+ln -sf /usr/bin/iscsiadm /sbin/iscsiadm
+
 systemctl enable iscsid
 systemctl start iscsid
 
