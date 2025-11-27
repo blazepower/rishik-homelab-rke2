b63038240cf9917d6af58d4cc973ef2d80291c70
Rishik Hombal <hombalrishik@gmail.com>
2025-11-27 14:34:23 -0800

    Add sudo to iscsiadm symlink commands

--- infrastructure/node-bootstrap/iscsi/iscsi-bootstrap.sh
+++ infrastructure/node-bootstrap/iscsi/iscsi-bootstrap.sh
@@ -8,8 +8,8 @@ if ! dpkg -s open-iscsi >/dev/null 2>&1; then
   apt-get install -y open-iscsi
 fi
 
-ln -sf /usr/bin/iscsiadm /usr/sbin/iscsiadm
-ln -sf /usr/bin/iscsiadm /sbin/iscsiadm
+sudo ln -sf /usr/bin/iscsiadm /usr/sbin/iscsiadm
+sudo ln -sf /usr/bin/iscsiadm /sbin/iscsiadm
 
 systemctl enable iscsid
 systemctl start iscsid
